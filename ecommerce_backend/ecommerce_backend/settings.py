091f332142b83fd5e4bf7d4b212ee2329f77bbee
TAkshayKumar09 <akshaykumart0609@gmail.com>
2025-11-27 17:42:41 +0530

    SecondTime commit

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-j!=%h$=6j9f%@y*7eg=^okt9&yu=xt)9lv4g2vz3b!32g+xk_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
