28e572d8d29bc29bbf33dbd953eb82f42d6909a0
TAkshayKumar09 <akshaykumart0609@gmail.com>
2025-11-30 14:29:23 +0530

    Added Corsheaders

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usersapp',
-    'cloudinary'
+    'cloudinary',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'ecommerce_backend.urls'
 
